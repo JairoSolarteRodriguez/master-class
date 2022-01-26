5b43e36ae50b59871b972db498f509b1bfd25d5f
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-01-26 09:28:45 -0500

    paginacion

--- src/components/ListOfCharacters.jsx
+++ src/components/ListOfCharacters.jsx
@@ -1,34 +1,38 @@
 import { useState, useEffect } from 'react'
 import { getAPI } from "../services/getApi"
 import { Character } from './Character';
+import Button from './Button/Button';
 
 const ListOfCharacters = () => {
   const [ characters, setCharacters ] = useState([]);
   const [ page, setPage ] = useState(1);
 
   useEffect(() =>{
-    setTimeout(() => {
-      getAPI(page).then( (character) => setCharacters(character))
-    }, 2000)
+    getAPI(page).then( async (character) => setCharacters(character))
   }, [ page ])
 
   const next = () =>{
     setCharacters([]);
     setPage(page + 1)
+  }
 
-    console.log(page)
+  const prev = () =>{
+    setCharacters([]);
+    setPage(page - 1)
   }
   
   if(characters.length < 1) return <h1>Cargando...</h1>
 
   return(
     <>
-      <button onClick={next}>next</button>
+      {page === 1 ? '' : <Button value='Prev' action={prev}/> }
+      {page === 42 ? '' : <Button value='Next' action={next}/> }
       {
-        characters.map(({ id, name, image, gender, species, status }) =>(
+        characters.map(({ id, name, image, gender, species, status }) => (
           <Character name={name} image={image} gender={gender} species={species} status={status} key={id}/>
         ))
       }
+
     </>
   )
 }
