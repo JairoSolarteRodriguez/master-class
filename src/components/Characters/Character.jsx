0b25e41eaa48a6c0760d1b7050a36c25ab30e65e
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-01-27 14:22:19 -0500

    somethings styles

--- src/components/Characters/Character.jsx
+++ src/components/Characters/Character.jsx
@@ -2,18 +2,13 @@ import { Link } from 'react-router-dom'
 import style from './Character.module.css'
 
 export const Character = (props) => {
-  const { id, name, status, species, gender, image } = props
+  const { id, name, image } = props
 
   return(
     <Link to={`/characters/${id}`} className={style.link}>
       <section className={style.card}>
         <h4 className={style.card_header}>{name}</h4>
         <img src={image} alt={name} />
-        <div className={style.info}>
-          <p className={style.text}><span className={style.bold}>Estado:</span> {status}</p>
-          <p className={style.text}><span className={style.bold}>Especie:</span> {species}</p>
-          <p className={style.text}><span className={style.bold}>Genero:</span> {gender}</p>
-        </div>
       </section>
     </Link>
   )
