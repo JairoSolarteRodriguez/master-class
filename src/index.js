64e190a93f654dd5ba6a12937dc0c8e21c49d0db
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-01-27 14:41:59 -0500

    Intento de correcccion de produccion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Navbar from "./components/Navbar/Navbar"
+import ListOfCharacters from "./components/Characters/ListOfCharacters"
+import OneCharacter from "./components/Characters/OneCharacter"
+import Home from "./components/Home/Home"
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='characters' element={<ListOfCharacters />} />
+          <Route path='/characters/:charId' element={<OneCharacter />} />
+        </Routes>
+      </Router>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
