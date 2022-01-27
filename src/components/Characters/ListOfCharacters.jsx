b3493a5ade657c04d4cf1647879042f2d31c9340
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-01-26 19:25:11 -0500

    preservacion de la informacion de la pagina

--- src/components/Characters/ListOfCharacters.jsx
+++ src/components/Characters/ListOfCharacters.jsx
@@ -7,30 +7,33 @@ import Button from '../Button/Button';
 import style from './Character.module.css'
 
 const ListOfCharacters = () => {
-  const [ characters, setCharacters ] = useState([]);
-  const [ page, setPage ] = useState(1);
 
+  const [ characters, setCharacters ] = useState([]);
+  const [ page, setPage ] = useState(localStorage.getItem('page'));
+  
   useEffect(() =>{
     getAPI(page).then( async (character) => setCharacters(character))
   }, [ page ])
 
   const next = () =>{
     setCharacters([]);
-    setPage(page + 1)
+    setPage(parseInt(page) + 1)
+    localStorage.setItem('page', parseInt(page) + 1);
   }
 
   const prev = () =>{
     setCharacters([]);
-    setPage(page - 1)
+    setPage(parseInt(page) - 1)
+    localStorage.setItem('page', parseInt(page) - 1);
   }
-  
+
   if(characters.length < 1) return <h1>Cargando...</h1>
 
   return(
     <section>
       <div className={style.containerButton}>
-        {page === 1 ? null : <Button value='Prev' action={prev}/> }
-        {page === 42 ? null : <Button value='Next' action={next}/> }
+        {page === 1 || page === '1' ? null : <Button value='Prev' action={prev}/> }
+        {page === 42 || page === '42' ? null : <Button value='Next' action={next}/> }
       </div>
       <div className={style.flexContainer}>
         {
